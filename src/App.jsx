298cf1556c652d60f5ade50c21841e46e29ed7a0
aaaanant <anantbhatt789@gmail.com>
2025-08-31 20:18:43 +0530

    aaaanant

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css'
+import {useState} from 'react';
+function App() {
+  const [board, setBoard] = useState(["", "X", "", "o", "", "", "X", "", ""]);
+ 
+
+  return (
+  <>
+ 
+  
+
+
+    <div className='container'>
+
+    <div className='heading'>
+      <h2>Tic Tac Toe</h2>
+
+    </div>
+
+    <div className='parent'>
+{board.map((value,index)=>(
+ <div key={index}className='box'>{value}</div>
+))}
+   
+   
+
+    </div>
+
+    <div className='playername'>
+      <h2>Next Player:X </h2>
+    </div>
+    <button className='restart'>Restart</button>
+
+
+    </div>
+
+
+
+    
+   
+    </>
+  )
+}
+
+export default App
